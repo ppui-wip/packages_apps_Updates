7b75dd3ab43f41812084127f4c7c305d45f04573
KostyaJRZ <jrzroyale@gmail.com>
2021-02-16 23:28:52 +0200

    Updater: Initalize PixelPlusUI Updater

--- Android.mk
+++ Android.mk
@@ -28,12 +28,12 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_REQUIRED_MODULES := privapp_whitelist_org.pixelexperience.ota.xml
+LOCAL_REQUIRED_MODULES := privapp_whitelist_org.pixelplusui.ota.xml
 
 include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp_whitelist_org.pixelexperience.ota.xml
+LOCAL_MODULE := privapp_whitelist_org.pixelplusui.ota.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
